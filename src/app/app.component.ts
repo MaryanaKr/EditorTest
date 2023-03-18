4c727c57f8f5bbde471bba4cded91b2e3efde634
maryana <maryana.k@nerdysoft.com>
2023-03-18 22:09:11 +0200

    added froala editor config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {FroalaEditorDirective} from "angular-froala-wysiwyg";
+//import FroalaEditor from 'froala-editor';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'EditorTest3';
+export class AppComponent implements OnInit{
+  title = 'EditorTest';
+
+  editor: any
+
+  ngOnInit() {
+    this.editor = {
+      toolbarButtons: ['fullscreen', 'bold', 'italic', 'underline', 'strikeThrough', 'subscript', 'superscript', '|',
+        'fontFamily', 'fontSize', 'color', 'inlineStyle', 'paragraphStyle', '|',
+        'paragraphFormat', 'align', 'formatOL', 'formatUL', 'outdent', 'indent', 'quote', '-',
+        'insertLink', 'insertImage', 'insertVideo', 'insertFile', 'insertTable', '|',
+        'emoticons', 'specialCharacters', 'insertHR', 'selectAll', 'clearFormatting', '|',
+        'print', 'getPDF', 'help', 'html', '|', 'undo', 'redo','trackChanges','markdown']
+    };
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {FroalaEditorModule, FroalaViewModule} from "angular-froala-wysiwyg";
+// Import all Froala Editor plugins.
+ import 'froala-editor/js/plugins.pkgd.min.js';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FroalaEditorModule.forRoot(), FroalaViewModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
